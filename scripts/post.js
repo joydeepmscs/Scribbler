5b7a1d9409cff5c045bd325f17bfde938d7f66ca
joydeepmscs <joydeep.mscs@gmail.com>
2021-03-07 18:50:27 +0000

    feature3- create post my changes

---
+++ scripts/post.js
@@ -0,0 +1,54 @@
+var postTitle = document.getElementById("post-header");
+var postContent = document.getElementById("content");
+var numberOfClick = 0;
+
+function myFunction(button) {
+  if (postTitle.contentEditable == "true") {
+    postTitle.contentEditable = "false";
+    postContent.contentEditable = "false";
+    postTitle.style.border = "none";
+    postContent.style.border = "none";
+    button.innerHTML = "Edit <i class=\"fa fa-pencil-square-o\"></i>";
+    postTitle.classList.remove("editable");
+    postContent.classList.remove("editable");
+
+  } else {
+    postTitle.contentEditable = "true";
+    postContent.contentEditable = "true";
+    postTitle.style.border = "1px solid pink";
+    postContent.style.border = "1px solid pink";
+    button.innerHTML = "Save <i class=\"fa fa-floppy-o\"></i>";
+    postTitle.classList.add("editable");
+    postContent.classList.add("editable");
+
+  }
+}
+
+
+function addLike() {
+  var likeButton = document.getElementById("add-likes");
+  likeButton.innerHTML =
+    '<i class="fa fa-thumbs-up ">' + "</i>" + "&nbsp;" + "Liked!";
+  likeButton.style.border = "none";
+  numberOfClick += 1;
+  if (numberOfClick != 0) {
+    if (numberOfClick == 1) {
+      document.getElementById("like-count").innerHTML =
+        numberOfClick + " person likes this!";
+    } else {
+      document.getElementById("like-count").innerHTML =
+        numberOfClick + " people likes this!";
+    }
+  }
+}
+
+function addComment(comment) {
+  if (comment.value.length > 0) {
+    document.getElementById("comment-list").innerHTML +=
+      '<p id="comment-posted">' + comment.value + "</p>";
+    var userComment = document.getElementById("userComment");
+    userComment.value = userComment.defaultValue;
+  } else {
+    alert("Enter some comment");
+  }
+}
