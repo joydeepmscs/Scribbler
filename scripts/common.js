14c61499ea6a23e7d2bfb658faad100221a9b36b
joydeepmscs <65868927+joydeepmscs@users.noreply.github.com>
2021-03-08 20:01:19 +0000

    Update common.js
    making header IE compatible

--- scripts/common.js
+++ scripts/common.js
@@ -24,64 +24,62 @@ function hideSignIn() {
 onLoadHeader();
 function onLoadHeader() {
     var headerTemplate=document.getElementsByClassName("header")[0];
-    headerTemplate.innerHTML = `<div class="header-container">
-        <p class="title">ScriBBler</p>
-        <p class="subheading">Explore, Imagine, Create</p>
-    </div>
-    <div class="header-btn-container">
-        <button class="signup-btn" onclick="showSignUp()">Sign Up</button>
-        <button class="signin-btn" onclick="showSignIn()">Sign In</button>
-    </div>
+    headerTemplate.innerHTML = '<div class="header-container">'+
+        '<p class="title">ScriBBler</p>'+
+        '<p class="subheading">Explore, Imagine, Create</p>'+
+    '</div>'+
+    '<div class="header-btn-container">'+
+        '<button class="signup-btn" onclick="showSignUp()">Sign Up</button>'+
+        '<button class="signin-btn" onclick="showSignIn()">Sign In</button>'+
+    '</div>'+
 
 <!-- Sign Up Modal -->
-<div id="signup-modal" class="modal">
-    <div class="modal-content">
-        <div class="modal-head-container">
-            <h2 class="modal-title">Get Started</h2>
-            <span class="modal-close" onclick="hideSignUp()">
-            <i class="fa fa-times" aria-hidden="true"></i>
-          </span>
-        </div>
-        <hr/>
-        <div class="modal-body">
-            <form id="signup-form">
-                <label for="name" class="label-text">Name</label>
-                <input type="text" placeholder="Enter your name" id="name" required/>
-                <label for="username" class="label-text">Username</label>
-                <input type="text" placeholder="Enter your username" id="username" required/>
-                <label for="password" class="label-text">Password</label>
-                <input type="password" placeholder="Enter your password" id="password" required/>
-                <label for="confirmpassword" class="label-text">Confirm Password</label>
-                <input type="password" placeholder="Re-enter your password" id="confirmpassword" required/>
-                <button type="submit" class="modal-popup-btn">Sign Up</button>
-            </form>
-        </div>
-    </div>
-</div>
+'<div id="signup-modal" class="modal">'+
+    '<div class="modal-content">'+
+        '<div class="modal-head-container">'+
+            '<h2 class="modal-title">Get Started</h2>'+
+            '<span class="modal-close" onclick="hideSignUp()">'+
+            '<i class="fa fa-times" aria-hidden="true"></i>'+
+          '</span>'+
+        '</div>'+
+        '<hr/>'+
+        '<div class="modal-body">'+
+            '<form id="signup-form">'+
+                '<label for="name" class="label-text">Name</label>'+
+                '<input type="text" placeholder="Enter your name" id="name" required/>'+
+                '<label for="username" class="label-text">Username</label>'+
+                '<input type="text" placeholder="Enter your username" id="username" required/>'+
+                '<label for="password" class="label-text">Password</label>'+
+                '<input type="password" placeholder="Enter your password" id="password" required/>'+
+                '<label for="confirmpassword" class="label-text">Confirm Password</label>'+
+                '<input type="password" placeholder="Re-enter your password" id="confirmpassword" required/>'+
+                '<button type="submit" class="modal-popup-btn">Sign Up</button>'+
+            '</form>'+
+        '</div>'+
+    '</div>'+
+'</div>'+
 <!-- Sign in Modal-->
-<div id="signin-modal" class="modal">
-    <div class="modal-content">
-        <div class="modal-head-container">
-            <h2 class="modal-title">Welcome Back!</h2>
-            <span class="modal-close" onclick="hideSignIn()">
-            <i class="fa fa-times" aria-hidden="true"></i>
-          </span>
-        </div>
-        <hr/>
-        <div class="modal-body">
-            <form id="signin-form">
-                <label for="signinusername" class="label-text">Username</label>
-                <input type="text" placeholder="Enter your username" id="signinusername" required/>
-                <label for="signinpassword" class="label-text">Password</label>
-                <input type="password" placeholder="Enter your password" id="signinpassword" required/>
-                <button type="submit" class="modal-popup-btn">Sign In</button>
-                <p class="bottom-banner">
-                    Not a member?
-                    <span class="bottom-banner-text" onclick="hideSignIn();showSignUp()">Sign Up</span>
-                </p>
-            </form>
-        </div>
-    </div>
-</div>`;
-
-}
+'<div id="signin-modal" class="modal">'+
+    '<div class="modal-content">'+
+        '<div class="modal-head-container">'+
+            '<h2 class="modal-title">Welcome Back!</h2>'+
+            '<span class="modal-close" onclick="hideSignIn()">'+
+            '<i class="fa fa-times" aria-hidden="true"></i>'+
+          '</span>'+
+        '</div>'+
+        '<hr/>'+
+        '<div class="modal-body">'+
+            '<form id="signin-form">'+
+                '<label for="signinusername" class="label-text">Username</label>'+
+                '<input type="text" placeholder="Enter your username" id="signinusername" required/>'+
+                '<label for="signinpassword" class="label-text">Password</label>'+
+                '<input type="password" placeholder="Enter your password" id="signinpassword" required/>'+
+                '<button type="submit" class="modal-popup-btn">Sign In</button>'+
+                '<p class="bottom-banner">'+
+                    'Not a member?'+
+                    '<span class="bottom-banner-text" onclick="hideSignIn();showSignUp()">Sign Up</span>'+
+                '</p>'+
+            '</form>'+
+        '</div>'+
+    '</div>'+
+'</div>';
